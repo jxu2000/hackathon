2e4440cb161e92de3eff257f2b559f992e494d1e
JImmy Xu <jimmyxu@Matthews-MacBook-Pro.local>
2019-04-26 10:31:05 -0400

    added signup page

--- AWMurals/src/app/app.module.ts
+++ AWMurals/src/app/app.module.ts
@@ -9,6 +9,21 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import * as firebase from 'firebase';
+
+firebase.initializeApp({
+  apiKey: "AIzaSyA63N0-GMrzMrrqZ0eiST3pA0DF7xnMsjM",
+    authDomain: "hackathon-c6bbc.firebaseapp.com",
+    databaseURL: "https://hackathon-c6bbc.firebaseio.com",
+    projectId: "hackathon-c6bbc",
+    storageBucket: "hackathon-c6bbc.appspot.com",
+    messagingSenderId: "1078477850584"
+});
+
+firebase.firestore().settings({
+  timestampsInSnapshots: true
+});
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
